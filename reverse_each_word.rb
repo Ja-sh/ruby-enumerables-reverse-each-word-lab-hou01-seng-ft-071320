3cdb3937b79ae678a5f15cdbcd1a7da89927559f
Joshua Moore <morejoshua99@hotmail.com>
2020-06-10 23:31:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,6 @@
-require pry
-
 array = []
 def reverse_each_word(string)
-  array = string.split(" ")
-  binding 'pry'
-  array.each do |element|
-  element.reverse
+  string.split(" ").each do |element|
+  array << element.reverse
 end
 end
